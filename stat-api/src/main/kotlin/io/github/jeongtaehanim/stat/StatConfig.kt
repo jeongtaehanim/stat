be64f4d8739ef3a29ddc084b4b7d9a38c799eff5
정태한 <jeongtaehanim@gmail.com>
2026-03-06 20:46:56 +0900

    🔨 StatConfig.potential 기본값 실수

--- stat-api/src/main/kotlin/io/github/jeongtaehanim/stat/StatConfig.kt
+++ stat-api/src/main/kotlin/io/github/jeongtaehanim/stat/StatConfig.kt
@@ -9,7 +9,7 @@ data class StatConfig(
     val deviation: Long = 0L,
     // potential = conf.{random(potential)}
     // base += event.{action} * conf.{potential}
-    val potential: Long = 0L
+    val potential: Long = 1L
 ) {
     fun rollBase(random: kotlin.random.Random = kotlin.random.Random): Long {
         return base + random.nextLong(-deviation, deviation + 1)
